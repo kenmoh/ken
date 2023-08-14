434a76b8ae3c283a0fa4e1b2535d607206108e5d
kenmoh <kenneth.aremoh@gmail.com>
2023-08-14 05:24:06 +0100

    update app

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,12 @@ import Social from "./components/Social";
 import Tech from "./components/Tech";
 import { techs, projects, socials } from "./constants";
 
+const python = ".assets/python.png";
+const rn = ".assets/React Native.png";
+const reservation = ".assets/reservation.jpg";
+const js = "./assets/JS.png";
+const docker = "./assets/Docker.png";
+
 function App() {
   return (
     <>
@@ -29,9 +35,10 @@ function App() {
           <div className="tech-container" id="technologies">
             <h3>TECHNOLOGIES</h3>
             <div className="technologies">
-              {techs.map((tech, index) => (
-                <Tech key={index} iconUrl={tech.icon} iconName={tech.name} />
-              ))}
+              <Tech iconUrl={python} iconName={"Python"} />
+              <Tech iconUrl={js} iconName={"JavaScript"} />
+              <Tech iconUrl={rn} iconName={"React Native"} />
+              <Tech iconUrl={docker} iconName={"Docker"} />
             </div>
           </div>
           <div className="projects-container" id="projects">
